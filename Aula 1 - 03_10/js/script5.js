161a9c044a81c2cbbf6cca230d060a4ef23ad589
Jeniffer Silva <112019233+JenifferSilva1@users.noreply.github.com>
2022-12-16 04:32:00 -0300

    Delete Aula 1 - 03_10 directory

--- Aula 1 - 03_10/js/script5.js
+++
@@ -1,7 +0,0 @@
-function soma(){
-    var a=10;
-    var b=20;
-    var soma=a+b;
-    alert("O valor da soma corresponde a :"+soma);
-
-}
--- Aula 1 - 03_10/js/soma.js
+++
@@ -1,8 +0,0 @@
-function soma(){
-    // cria caixa de texto para receber um valor
-    // parseInt torna o número inteiro e deixa de somar strings
-    var numero1=parseInt(prompt("Digite um número :"));
-    var numero2=parseInt(prompt("Digite um número. :"));
-    var soma =numero1+numero2;
-    alert("O valor da soma é :"+soma);
-}
