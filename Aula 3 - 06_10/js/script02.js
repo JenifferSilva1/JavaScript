439984e1d960a64057db1d9aeb922e5d2dfdad91
Jeniffer Silva <112019233+JenifferSilva1@users.noreply.github.com>
2022-12-16 04:32:21 -0300

    Delete Aula 3 - 06_10 directory

--- Aula 3 - 06_10/js/script02.js
+++
@@ -1,9 +0,0 @@
-var numero = parseInt(prompt("Digite um número: "));
-var contadora = 1;
-while (contadora <= 10) {
-    document.write(numero + "*" + contadora + "=" + contadora * numero + "</br>"); 
-    contadora++;
-}
-
-//document.write(numero + "*" + contadora + "=" ---- como se estivesse escrevendo a conta - ex: 3*1=
-// "=" + contadora * numero + "</br>"); ---- resolução da conta
